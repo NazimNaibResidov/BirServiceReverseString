dbf5a103d3343e2874b813f18bb863d912fd9e05
Naib Residov <residovnaib@gmail.com>
2019-11-18 22:35:37 +0200

    addedFirst

---
+++ Web.AppService/ReverseService.asmx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Web.AppService
+{
+  
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    
+    public class ReverseService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string ReverseString(string word)
+        {
+            string newSoz = Tools.ReverseString(word);
+            return newSoz;
+        }
+    }
+}
---
+++ Web.AppService/Tools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.AppService
+{
+    public class Tools
+    {
+        public static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
